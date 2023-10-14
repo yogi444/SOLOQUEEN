46f017d51e2bed0b93a8a4a218cb8828ef50d7a1
Mr.Rolex <139060034+yogi444@users.noreply.github.com>
2023-10-15 03:48:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1385,8 +1385,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-            InputMediaPhoto(random.choice(https://graph.org/file/46fd33d87206259d3071b.jpg))
-        )
+            InputMediaPhoto(https://graph.org/file/46fd33d87206259d3071b.jpg)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
