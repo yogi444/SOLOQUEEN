9090cd963bd5790ebe9000e3927bce0d4f94b85f
Mr.Rolex <139060034+yogi444@users.noreply.github.com>
2023-10-15 04:42:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1757,7 +1757,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await client.edit_message_media(
                 query.message.chat.id, 
                 query.message.id, 
-                InputMediaPhoto(random.choice(PICS))
+                InputMediaPhoto(QR)
             )
             reply_markup = InlineKeyboardMarkup(btn)
             await query.message.edit_text(
